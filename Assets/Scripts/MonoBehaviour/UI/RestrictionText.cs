8b45132b890e83e914f756aaaa85e2f5dd004ce8
Matthew Seaward <matthewseaward@hotmail.co.uk>
2019-09-11 18:41:56 +0100

    Adding text detailing the restriction on game start

---
+++ Assets/Scripts/MonoBehaviour/UI/RestrictionText.cs
@@ -0,0 +1,59 @@
+﻿using Assets.Scripts.Workers.Score_and_Limits;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    class RestrictionText : MonoBehaviour
+    {
+        Animator animator;
+        Text text;
+        Image image;
+
+        public void Awake()
+        {
+
+            animator = GetComponent<Animator>();
+            text = GetComponentInChildren<Text>();
+            image = GetComponent<Image>();
+
+            HideText();
+
+            LevelStart.GameStarted += ShowText;
+        }
+
+        public void OnDestroy()
+        {
+            LevelStart.GameStarted -= ShowText;
+        }
+
+        public void ShowText()
+        {
+            var currentRestriction = LevelManager.Instance.SelectedLevel.GetCurrentRestriction();
+            if (currentRestriction is NoRestriction)
+            {
+                return;
+            }
+            
+            SetEnabled(true);
+            text.text = currentRestriction.GetRestrictionText();;
+            animator.SetTrigger("Show Restriction");
+        }
+
+        public void HideText()
+        {
+            SetEnabled(false);
+        }
+
+        private void SetEnabled(bool enabled)
+        {
+            image.enabled = enabled;
+
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(enabled);
+            }
+            animator.enabled = enabled;
+        }
+    }
+}
