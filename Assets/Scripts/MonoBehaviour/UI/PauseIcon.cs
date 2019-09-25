f27b5916683f3de2e7a36b609d88226a93f4999d
Matthew Seaward <matthewseaward@hotmail.co.uk>
2019-09-25 18:39:41 +0100

    Added a simple pause icon

---
+++ Assets/Scripts/MonoBehaviour/UI/PauseIcon.cs
@@ -0,0 +1,25 @@
+﻿using Assets.Scripts.Managers;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    class PauseIcon : MonoBehaviour
+    {
+        private void Awake()
+        {
+            GameManager.PauseStateChanged += PauseStateChanged;
+            gameObject.SetActive(false);
+        }
+
+        private void OnDestroy()
+        {
+            GameManager.PauseStateChanged -= PauseStateChanged;
+
+        }
+
+        private void PauseStateChanged(bool gamePaused)
+        {
+            gameObject.SetActive(gamePaused && !MenuProvider.Instance.OnDisplay);
+        }
+    }
+}
