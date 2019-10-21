9b6a9db5dfd73beb520022b08bcecda6b9cd746a
Matthew Seaward <matthewseaward@hotmail.co.uk>
2019-10-21 09:34:13 +0100

    Modified the Cancel button so it's now static

--- Assets/Scripts/MonoBehaviour/UI/CancelButton.cs
+++ Assets/Scripts/MonoBehaviour/UI/CancelButton.cs
@@ -4,8 +4,6 @@ namespace Assets.Scripts.Game_Components
 {
     class CancelButton : MonoBehaviour
     {
-        private Vector3 startPos;
-
         [SerializeField]
         private Sprite defaultSprite;
 
@@ -15,27 +13,27 @@ namespace Assets.Scripts.Game_Components
         private bool mouseOver;
 
         [SerializeField]
-        private float maxDistance = 1;
-
-        [SerializeField]
-        private float distanceToStart = 3;
-
-        [SerializeField]
-        private float distanceToEnd = 0.3f;
         
         private void Start()
         {
-            startPos = transform.position;
+            PieceSelectionManager.SelectedPiecesChanged += PieceSelectionManager_SelectedPiecesChanged;
             OnMouseExit();
+        }
+
+        private void OnDestroy()
+        {
+            PieceSelectionManager.SelectedPiecesChanged -= PieceSelectionManager_SelectedPiecesChanged;
+        }
+
+        private void PieceSelectionManager_SelectedPiecesChanged(System.Collections.Generic.LinkedList<ISquarePiece> pieces)
+        {
+            GetComponentInChildren<Animator>().SetBool("Enabled", Input.GetMouseButton(0) && PieceSelectionManager.Instance.CurrentPieces.Count >= 2);
         }
 
         private void Update()
         {
             if (Input.GetMouseButtonUp(0))
             {
-                transform.position = startPos;
-                GetComponentInChildren<Animator>().SetBool("Enabled", false);
-
                 if (mouseOver)
                 {
                     PieceSelectionManager.Instance.ClearCurrentPieces();
@@ -43,27 +41,11 @@ namespace Assets.Scripts.Game_Components
                 }
             }
 
-            if (!Input.GetMouseButton(0) || PieceSelectionManager.Instance.CurrentPieces.Count < 2)
-            {
-                return;
-            }
+            //if (!Input.GetMouseButton(0) || PieceSelectionManager.Instance.CurrentPieces.Count < 2)
+            //{
+            //    return;
+            //}
 
-            GetComponentInChildren<Animator>().SetBool("Enabled", true);
-
-            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mousePos = new Vector3(mousePos.x, mousePos.y, 0);
-            if (Vector3.Distance(startPos, mousePos) > distanceToStart)
-            {
-                transform.position = startPos;
-                return;
-            }
-
-            if (Vector3.Distance(transform.position, mousePos) < distanceToEnd)
-            {
-                return;
-            }
-
-            transform.position = Vector3.MoveTowards(startPos, mousePos, maxDistance * Time.deltaTime);
         }
 
         private void OnMouseEnter()
